fcd134a5596ea9212b6d42318d2472c1307dc4df
BleuSkell <kyasowi1@gmail.com>
2025-04-10 10:58:07 +0200

    modified userFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,7 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'role' => $this->faker->randomElement(['employee', 'customer']),
+            'role' => $this->faker->randomElement(['worker', 'user', 'admin']),
             'last_login' => now(),
             'remember_token' => Str::random(10),
         ];
