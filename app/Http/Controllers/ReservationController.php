050d5383a971b9401ca5053f8a847d7bc7a14b4e
estellemsastro <estelle2007-sastropawiro@outlook.com>
2025-04-10 16:56:01 +0200

    ik krijg de error bij edit en create niet weg door te wijnig tijd

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Reservation;
 use App\Models\Lane;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB; 
 class ReservationController extends Controller
 {
     public function index()
